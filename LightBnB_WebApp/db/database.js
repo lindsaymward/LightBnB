5f9fd29bc104893d8665d35875264016b21edcaf
Lindsay Ward <lward23@hotmail.com>
2023-06-01 16:25:37 -0700

    Complete database add for addProperty form

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -115,7 +115,7 @@ const getAllProperties = function(options, limit = 10) {
   SELECT properties.*, AVG(property_reviews.rating) AS average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
-  `
+  `;
 
   if (options.city) {
     queryParams.push(`%${options.city}%`);
@@ -129,7 +129,7 @@ const getAllProperties = function(options, limit = 10) {
 
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
     queryParams.push((options.minimum_price_per_night * 100), (options.maximum_price_per_night * 100));
-    queryString += `AND cost_per_night >= $${queryParams.length-1} AND cost_per_night <= $${queryParams.length} `;
+    queryString += `AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
   }
 
   queryString += `GROUP BY properties.id `;
@@ -140,7 +140,7 @@ const getAllProperties = function(options, limit = 10) {
   }
 
   queryParams.push(limit);
-  
+
   queryString += `
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
@@ -160,10 +160,33 @@ const getAllProperties = function(options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const propertyParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+  return pool.query(
+    `INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;`, propertyParams
+  )
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 module.exports = {
